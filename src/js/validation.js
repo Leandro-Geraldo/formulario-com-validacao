03320f86084767fa52d8f62160d058487a4b2151
Leandro Geraldo <leandro.a.pd4@gmail.com>
2022-12-16 22:14:14 -0300

    limpeza do codigo, e correção de funcionalidades.

--- src/js/validation.js
+++ src/js/validation.js
@@ -1,29 +1,26 @@
-const campos = document.querySelectorAll(".inputs")
 const campoInput = document.querySelectorAll(".input")
 const btnSubmit = document.querySelector(".submit")
 
+btnSubmit.addEventListener('click', (event)=>{
+  event.preventDefault()
 
-btnSubmit.addEventListener("click", (event)=>{
-    event.preventDefault()
-    
-    
-    campoInput.forEach(input => {
-        campos.forEach( i=>{
-            if(input.value === ""){
-                input.classList.add("error")
-                i.classList.add("span-error")
-            }else{
-                i.classList.remove("span-error")
-                input.classList.remove("error");
-            }
-        })
-    })
+  campoInput.forEach(input =>{
+    if (input.value !== '') {
+      input.parentElement.classList.remove('span-error')
+      input.classList.remove('error')
+
+    }else{
+      input.parentElement.classList.add('span-error')
+      input.classList.add('error')
+    }
+  })
 })
 
 campoInput.forEach(input => {
     input.addEventListener('change', () => {
       if (input.value !== "") {
         input.classList.add('preenchido')
+        input.parentElement.classList.remove('span-error')
       }
       else {
         input.classList.remove('preenchido')
